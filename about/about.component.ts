bfa177e4b540835de8213830c386d8bda7626a3f
gerryasher <gerryasher@gmail.com>
2016-07-07 23:02:05 -0700

    Delete about.component.ts

--- about/about.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    template: '<h3>{{pageTitle}}</h3>'
-})
-export class AboutComponent {
-    public pageTitle: string = "About this site";
-}
